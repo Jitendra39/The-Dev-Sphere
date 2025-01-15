6a9ebed0e154b541b4b92ee87f04181e004f3c3b
Jitendra39 <jeetendrachoudhari213@gmail.com>
2025-01-15 14:53:58 +0530

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -2,25 +2,27 @@ import withImages from "next-images";
 import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
-  dest: "public", // Specifies where the service worker and assets will be output
-  disable: false, // Disables PWA in development mode
-})(
-  withImages({
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**", // Allows all image domains with https
-        },
-        {
-          protocol: "http",
-          hostname: "**", // Allows all image domains with http
-        },
-      ],
-    },
-    // Additional Next.js configurations if needed
-  })
-);
+const nextConfig = withImages({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allows all image domains with https
+      },
+      {
+        protocol: "http",
+        hostname: "**", // Allows all image domains with http
+      },
+    ],
+  },
+  // Additional Next.js configurations if needed
+});
 
-export default nextConfig;
+const config =
+  process.env.NODE_ENV === "production"
+    ? withPWA({
+        dest: "public", // Specifies where the service worker and assets will be output
+      })(nextConfig)
+    : nextConfig;
+
+export default config;
